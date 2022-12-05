1d569912f5d81884924b0210a16e5beb60095427
xslvdrx <105146097+xslvdrx@users.noreply.github.com>
2022-12-05 20:16:23 +0100

    upgrade

--- script.js
+++ script.js
@@ -94,3 +94,14 @@ var productBig = new Swiper(".big-image", {
 		swiper: productThumb,
 	},
 });
+
+//stock products bar width percentage
+var stocks = document.querySelectorAll(".products .stock");
+for (let x = 0; x < stocks.length; x++) {
+	let stock = stocks[x].dataset.stock,
+		available = stocks[x].querySelector("qty-available").innerHTML,
+		sold = stocks[x].querySelector(".qty-sold").innerHTML,
+		percent = (sold * 100) / stock;
+
+	stocks[x].querySelector(".available").style.width = percent + "%";
+}
