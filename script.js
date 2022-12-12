30f0a6d8287a58d3d3545db9e5fa0f70cc093398
xslvdrx <105146097+xslvdrx@users.noreply.github.com>
2022-12-12 10:10:16 +0100

    Cart Overlay

--- script.js
+++ script.js
@@ -105,3 +105,24 @@ for (let x = 0; x < stocks.length; x++) {
 
 	stocks[x].querySelector(".available").style.width = percent + "%";
 }
+
+//show cart on click
+const divtoShow = ".mini-cart";
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector(".cart-trigger");
+
+divTrigger.addEventListener("click", () => {
+	setTimeout(() => {
+		if (!divPopup.classList.contains("show")) {
+			divPopup.classList.add("show");
+		}
+	}, 250);
+});
+
+//close by click outside
+document.addEventListener("click", (e) => {
+	const isClosest = e.target.closest(divtoShow);
+	if (!isClosest && divPopup.classList.contains("show")) {
+		divPopup.classList.remove("show");
+	}
+});
